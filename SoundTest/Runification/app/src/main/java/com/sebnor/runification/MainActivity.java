20e78f9a05145c4ae2f18f85f181e8ccd0eb7159
Sebkhi, Nordine <sebnor31@gatech.edu>
2017-07-11 20:18:05 -0400

    Configure Pure Data and load an interactive test patch

--- SoundTest/Runification/app/src/main/java/com/sebnor/runification/MainActivity.java
+++ SoundTest/Runification/app/src/main/java/com/sebnor/runification/MainActivity.java
@@ -5,15 +5,23 @@ import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.io.PdAudio;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+
 public class MainActivity extends AppCompatActivity {
     Random rand;
     private static final String TAG = "Sebnor";
-    MediaPlayer mediaPlayer;
+//    MediaPlayer mediaPlayer;
 
     private EditText hr;
     private EditText minHr;
@@ -22,13 +30,25 @@ public class MainActivity extends AppCompatActivity {
     private Handler timer;
     private Runnable runnable;
 
+    private void initPD() throws IOException {
+        int sampleRate = AudioParameters.suggestSampleRate();
+        PdAudio.initAudio(sampleRate, 0, 2, 8, true);
+    }
+
+    private void loadPdPatch() throws IOException {
+        File dir = getFilesDir();
+        IoUtils.extractZipResource(getResources().openRawResource(R.raw.test_1), dir, true);
+        File pdPatch = new File(dir, "test_1.pd");
+        PdBase.openPatch(pdPatch.getAbsolutePath());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         rand = new Random();
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.heartbeat);
+//        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.heartbeat);
         hr = (EditText)findViewById(R.id.hr);
         minHr = (EditText)findViewById(R.id.minEdit);
         maxHr = (EditText)findViewById(R.id.maxEdit);
@@ -36,6 +56,15 @@ public class MainActivity extends AppCompatActivity {
         minHr.setText(String.valueOf(60));
         maxHr.setText(String.valueOf(150));
 
+        // Init Pure Data
+        try{
+            initPD();
+            loadPdPatch();
+        } catch (IOException e){
+            Log.i(TAG, e.getMessage());
+        }
+
+        // Init timer
         timer = new Handler();
 
         runnable = new Runnable() {
@@ -50,26 +79,32 @@ public class MainActivity extends AppCompatActivity {
 
                 if (newHR < minHrVal){
                     hr.setTextColor(Color.BLUE);
-                    mediaPlayer.start();
+                    PdBase.sendFloat("onOff", 1.0f);
                 }
                 else if (newHR > maxHrVal){
                     hr.setTextColor(Color.RED);
-                    mediaPlayer.start();
+                    PdBase.sendFloat("onOff", 1.0f);
                 }
                 else {
                     hr.setTextColor(Color.BLACK);
+                    PdBase.sendFloat("onOff", 0.0f);
                 }
 
                 timer.postDelayed(this, 1000);
             }
         };
-
     }
 
     @Override
     public void onResume(){
         super.onResume();
+        PdAudio.startAudio(this);
+    }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        PdAudio.stopAudio();
     }
 
     public void onStartClick(View view){
