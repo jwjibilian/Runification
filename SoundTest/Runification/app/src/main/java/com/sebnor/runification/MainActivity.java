289f6199cacbf82b5f17c478fde57eb397098335
AD\nsebkhi3 <sebnor31@gatech.edu>
2017-07-13 12:03:38 -0400

    Add sonification for over hr. Add a text change listener for the HR instead of a random generator.

--- SoundTest/Runification/app/src/main/java/com/sebnor/runification/MainActivity.java
+++ SoundTest/Runification/app/src/main/java/com/sebnor/runification/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
+
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -21,15 +24,20 @@ import org.puredata.core.utils.IoUtils;
 public class MainActivity extends AppCompatActivity {
     Random rand;
     private static final String TAG = "Sebnor";
-//    MediaPlayer mediaPlayer;
+    MediaPlayer mediaPlayer;
 
     private EditText hr;
     private EditText minHr;
     private EditText maxHr;
 
+    private int prevHr;
+
     private Handler timer;
     private Runnable runnable;
 
+    /**********************
+     * Initialize Pure Data
+     ***********************/
     private void initPD() throws IOException {
         int sampleRate = AudioParameters.suggestSampleRate();
         PdAudio.initAudio(sampleRate, 0, 2, 8, true);
@@ -37,9 +45,46 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadPdPatch() throws IOException {
         File dir = getFilesDir();
-        IoUtils.extractZipResource(getResources().openRawResource(R.raw.test_1), dir, true);
-        File pdPatch = new File(dir, "test_1.pd");
+        IoUtils.extractZipResource(getResources().openRawResource(R.raw.pd_sonification), dir, true);
+        File pdPatch = new File(dir, "over_hr.pd");
         PdBase.openPatch(pdPatch.getAbsolutePath());
+    }
+
+    private void processHr(int newHr){
+
+        int minHrVal = Integer.parseInt(minHr.getText().toString());
+        int maxHrVal = Integer.parseInt(maxHr.getText().toString());
+
+        PdBase.sendFloat("emitAlert", 0.0f);
+
+        if (newHr < minHrVal){
+            hr.setTextColor(Color.BLUE);
+        }
+        else if (newHr > maxHrVal){
+            hr.setTextColor(Color.RED);
+            float level = 0.0f;
+
+            if ((newHr - maxHrVal) < 10){
+                level = 1.0f;
+            }
+            else if ((newHr - maxHrVal) < 20){
+                level = 2.0f;
+            }
+            else if ((newHr - maxHrVal) < 30){
+                level = 3.0f;
+            }
+            else if ((newHr - maxHrVal) < 40){
+                level = 4.0f;
+            }
+            else if ((newHr - maxHrVal) < 50){
+                level = 5.0f;
+            }
+            PdBase.sendFloat("alertLevel", level);
+            PdBase.sendFloat("emitAlert", 1.0f);
+        }
+        else {
+            hr.setTextColor(Color.BLACK);
+        }
     }
 
     @Override
@@ -47,14 +92,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rand = new Random();
-//        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.heartbeat);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.heartbeat);
+
+        // Set initial state of Heart rates values
         hr = (EditText)findViewById(R.id.hr);
         minHr = (EditText)findViewById(R.id.minEdit);
         maxHr = (EditText)findViewById(R.id.maxEdit);
 
+        prevHr = 100;
+        hr.setText(String.valueOf(prevHr));
         minHr.setText(String.valueOf(60));
         maxHr.setText(String.valueOf(150));
+
+        // Set a text listener for heart rate values
+        hr.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            public void afterTextChanged(Editable hr) {
+                int newHr;
+
+                try { newHr = Integer.parseInt(hr.toString());}
+                catch (NumberFormatException e){ newHr = prevHr;}
+
+                prevHr = newHr;
+                processHr(newHr);
+            }
+        });
+
 
         // Init Pure Data
         try{
@@ -65,40 +129,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Init timer
-        timer = new Handler();
+//        timer = new Handler();
 
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                int newHR = rand.nextInt(180) + 20; // 20 - 200
-                Log.i(TAG, "HR = " + newHR);
-                hr.setText(String.valueOf(newHR));
-
-                int minHrVal = Integer.parseInt(minHr.getText().toString());
-                int maxHrVal = Integer.parseInt(maxHr.getText().toString());
-
-                if (newHR < minHrVal){
-                    hr.setTextColor(Color.BLUE);
-                    PdBase.sendFloat("onOff", 1.0f);
-                }
-                else if (newHR > maxHrVal){
-                    hr.setTextColor(Color.RED);
-                    PdBase.sendFloat("onOff", 1.0f);
-                }
-                else {
-                    hr.setTextColor(Color.BLACK);
-                    PdBase.sendFloat("onOff", 0.0f);
-                }
-
-                timer.postDelayed(this, 1000);
-            }
-        };
+//        runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                timer.postDelayed(this, 1000);
+//            }
+//        };
     }
 
     @Override
     public void onResume(){
         super.onResume();
-        PdAudio.startAudio(this);
+//        PdAudio.startAudio(this);
     }
 
     @Override
@@ -108,11 +153,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStartClick(View view){
-        timer.postDelayed(runnable, 1000);
+//        timer.postDelayed(runnable, 1000);
+        PdAudio.startAudio(this);
     }
 
     public void onStopClick(View view){
-        timer.removeCallbacks(runnable);
+//        timer.removeCallbacks(runnable);
+        PdAudio.stopAudio();
     }
 
 }
