e829a396b0ed5ff2cc51766c638311ad7a7ee6a6
Joshua J <jwjibilian@gmail.com>
2017-07-14 18:07:10 -0400

    Bare Bone UI implemented

--- RunificationAndroid/app/src/main/java/com/example/jwjibilian/runificationandroid/view/TrainingOptions.java
+++ RunificationAndroid/app/src/main/java/com/example/jwjibilian/runificationandroid/view/TrainingOptions.java
@@ -1,7 +1,10 @@
 package com.example.jwjibilian.runificationandroid.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.jwjibilian.runificationandroid.R;
 
@@ -11,6 +14,32 @@ public class TrainingOptions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_options);
+
+
+        Button WLossButton = (Button) findViewById(R.id.WLossButton);
+        WLossButton.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), WeightLossTraining.class);
+                startActivity(myIntent);
+            }
+        });
+
+
+
+
+        Button ITrainingButton= (Button) findViewById(R.id.IntervalTrainingButton);
+        ITrainingButton.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), IntervalTraining.class);
+                startActivity(myIntent);
+            }
+        });
 
 
     }
