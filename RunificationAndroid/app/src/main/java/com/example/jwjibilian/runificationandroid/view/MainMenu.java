b464e3a1f478b7d5130f19bbf867b987160751dc
Joshua <jwjibilian@gmail.com>
2017-07-20 18:10:59 -0400

    user data save and load

--- RunificationAndroid/app/src/main/java/com/example/jwjibilian/runificationandroid/view/MainMenu.java
+++ RunificationAndroid/app/src/main/java/com/example/jwjibilian/runificationandroid/view/MainMenu.java
@@ -6,13 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.jwjibilian.runificationandroid.R;
+import com.example.jwjibilian.runificationandroid.controller.PebbleConnectivity;
+import com.example.jwjibilian.runificationandroid.model.User;
 
 public class MainMenu extends AppCompatActivity {
-
-
-
 
 
     @Override
@@ -20,18 +20,24 @@ public class MainMenu extends AppCompatActivity {
         return super.getApplicationContext();
     }
 
- /*   private String info = "TestComms";
+    private String info = "TestComms";
     private static TextView HR;
-    private PebbleConnectivity comms;*/
+    private PebbleConnectivity comms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
 
+        //comms = new PebbleConnectivity(getApplicationContext(), HR);
+        //comms.startApp(getApplicationContext());
+        //comms.startListener();
+
+        User user = User.getInstance(getApplicationContext());
+
         //User Info Button
         Button userInfo = (Button) findViewById(R.id.UserInfo);
-        userInfo.setOnClickListener(new View.OnClickListener(){
+        userInfo.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
@@ -44,19 +50,15 @@ public class MainMenu extends AppCompatActivity {
 
         //Training Button
         Button trainingOptions = (Button) findViewById(R.id.SetTrainingButton);
-            trainingOptions.setOnClickListener(new View.OnClickListener(){
+        trainingOptions.setOnClickListener(new View.OnClickListener() {
 
 
-                @Override
-                public void onClick(View view) {
-                    Intent myIntent = new Intent(view.getContext(), TrainingOptions.class);
-                    startActivity(myIntent);
-                }
-            });
-
-
-
-
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), TrainingOptions.class);
+                startActivity(myIntent);
+            }
+        });
 
 
         //this stuff is what I usded to get communications working with the watch
@@ -73,24 +75,16 @@ public class MainMenu extends AppCompatActivity {
     comms.startApp(getApplicationContext());*/
 
 
-
-
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         //Starts the communications with the watch
         //comms.startListener();
 
 
-
-
-
-
-
     }
-
 
 
 }
